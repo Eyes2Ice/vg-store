586e78ae3d759957c457e3220139969045b53380
Ruslan <jer_X_2014_projekt@mail.ru>
2026-07-02 12:20:46 +0300

    Catalog edited

--- src/Components/Catalog/Catalog.tsx
+++ src/Components/Catalog/Catalog.tsx
@@ -1,17 +1,73 @@
+import { useEffect, useState } from "react";
 import styles from "@/Components/Catalog/Catalog.module.css";
 import ky from "ky";
+import { Card, Image, Title, Flex } from "@mantine/core";
 
-const vegitables = await ky(
-  "https://res.cloudinary.com/sivadass/raw/upload/v1535817394/json/products.json",
-).json();
-
-console.log(vegitables);
+interface Vegitable {
+  id: number;
+  name: string;
+  price: number;
+  image: string;
+} // Типы пропов для продуктов
 
 const Catalog = () => {
+  const [vegitablesList, setVegitablesList] = useState<Vegitable[]>([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const data = await ky(
+          "https://res.cloudinary.com/sivadass/raw/upload/v1535817394/json/products.json",
+        ).json<Vegitable[]>();
+
+        setVegitablesList(data);
+      } catch (error) {
+        if (error instanceof Error) {
+          console.log(error.message);
+        } else {
+          console.log(String(error));
+        }
+      }
+    };
+    getData();
+  }, []); // Запрос продуктов
+
   return (
-    <section className={styles.catalog}>
-      <h1 className={styles.catalog__title}>Catalog</h1>
-      <ul className={styles.catalog__list}></ul>
+    <section>
+      {/* Заголовок */}
+      <Title
+        order={1}
+        mb={49}
+        fw={600}
+        size={32}
+        className={styles.catalog__title}
+      >
+        Catalog
+      </Title>
+      {/* Каталог */}
+      <Flex component="ul" rowGap={28} columnGap={24} wrap="wrap">
+        {vegitablesList.map((vegitable) => {
+          return (
+            <Card
+              component="li"
+              key={vegitable.id}
+              w={302}
+              py={16}
+              pis={16}
+              pie={10}
+              className={styles.catalog__item}
+              bdrs={24}
+            >
+              <Image
+                src={vegitable.image}
+                w={276}
+                height={276}
+                alt={vegitable.name}
+              />
+            </Card>
+          );
+        })}
+      </Flex>
     </section>
   );
 };
